8bf8c96a023d8b626a73c5eb1176470016346a73
Henok Mekonnen <henatan99@yahoo.com>
2021-10-18 22:56:42 +0300

    Add AddTodoComponent component in components folder

---
+++ src/components/addTodo.components.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class AddTodoComponent extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
